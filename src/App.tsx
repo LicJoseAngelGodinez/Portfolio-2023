254737d77b5c94296316f3b0c542dcb5c44a04c9
Dante Samael Fernández Lago <dantesamaelfernandezlago@gmail.com>
2023-01-18 10:38:51 -0500

    Added HashRouter for routing.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/home/home';
 import Test from './pages/test/test';
 import NotFound from './pages/notfound/not-found';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/test' element={<Test />} />
-      <Route path='*' element={<NotFound />} />
-    </Routes>
+    <HashRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/test' element={<Test />} />
+        <Route path='/*' element={<NotFound />} />
+      </Routes>
+    </HashRouter>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -10,8 +9,6 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-   <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );

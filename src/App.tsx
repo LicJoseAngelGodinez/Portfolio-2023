a4c6f5e634b708228c919d4692e2bb7f604048cf
Dante Samael Fernandez Lago <dantesamaelfernandezlago@gmail.com>
2023-01-18 17:28:53 -0500

    feature/custom-404 (#7)
    * Create: custom 404 page plus new icons using WEBP.
    
    * Change: PATH was updated to match build route.

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,10 @@ import Home from './pages/home/home';
 import Test from './pages/test/test';
 import NotFound from './pages/notfound/not-found';
 
+import './assets/icons-monkey/see-monkey.webp';
+import './assets/icons-monkey/hear-monkey.webp';
+import './assets/icons-monkey/speak-monkey.webp';
+
 function App() {
   return (
     <HashRouter>

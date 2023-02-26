9f0969dd284669ebbc692a64a59c670787acdf82
José Ángel <licangelgodinez@hotmail.com>
2023-02-26 13:40:38 -0500

    adding new changes (#18)
    Co-authored-by: Angel Ubuntu Desktop <joseangel.godinez1989@gmail.com>

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,5 +1,6 @@
 import monkey from '../../assets/icons-monkey/full-monkey.webp';
 import Banner from '../../components/banner/banner';
+import DesktopButton from '../../components/buttons/desktop-buttons/desktop-button';
 
 import styles from './home.module.css'
 
@@ -7,9 +8,9 @@ export default function Home() {
   return (
     <div className={styles.home}>
         <object data={monkey} className="logo" width="100" height="100"> </object>
-        <h1>{import.meta.env.VITE_DOMAIN}</h1>
         <Banner>
-            This is a WIP
+            <span>{import.meta.env.VITE_APP_DOMAIN}</span>
+            <DesktopButton text="This is a WIP" url="/#/test" />
         </Banner>
     </div>
   )

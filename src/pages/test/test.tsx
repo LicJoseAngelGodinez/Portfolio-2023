27c8b278439091515bdb526026e30792453da5d0
José Ángel <licangelgodinez@hotmail.com>
2023-04-21 23:36:08 -0500

    adding world time api, loader and refactor (#25)

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -1,19 +1,30 @@
 import { useEffect, useState } from 'react'
 import Banner from '../../components/banner/banner';
+import Loader from '../../components/loader/loader';
 import DesktopButton from '../../components/buttons/desktop-buttons/desktop-button';
-import { checkFence } from '../../utils/app-utils';
+import {
+  checkFence,
+  fetchTimezone,
+  getMapsUrl,
+  validTime,
+} from '../../utils/app-utils';
 
 export default function Test() {
+  const { VITE_APP_FIRST_FETCH_API_URL: apiUrl } = import.meta.env;
   const [errorMessage, setErrorMessage] = useState<string>("Wait please...");
   const [urlMaps, setUrlMaps] = useState<string>("");
-  const [validLocation, setValidLocation] = useState("");
+  const [validLocation, setValidLocation] = useState<string>("");
+  const [validSchedule, setValidSchedule] = useState<boolean>(false);
+  const {data,loading,error} = fetchTimezone(apiUrl);
 
   useEffect(() => {
     const success = (position:GeolocationPosition) => {
       const { coords: { latitude, longitude } } = position;    
       setValidLocation(checkFence({lat: latitude, lng: longitude}));
-      setUrlMaps(`https://www.google.com/maps/search/?api=1&query=${latitude}%2C${longitude}&hl=es-419`);
-      setErrorMessage("Click the link to check your location!");
+      setUrlMaps(getMapsUrl({latitude, longitude}));
+      console.log({url: getMapsUrl({latitude, longitude})});
+      
+      setErrorMessage("Click the link to check your location!");      
     }
 
     const errorFn = ({code, message}:any) => {
@@ -23,18 +34,33 @@ export default function Test() {
     navigator.geolocation.getCurrentPosition(success, errorFn);
   }, []);
 
+  useEffect(() => {
+    if(!loading){
+      setValidSchedule(validTime(data.date_time));      
+    }
+  },[loading]);
+
   return (
     <div className="container">
-      <Banner>
-        <span>{errorMessage}</span>
-        {validLocation && (<span>{validLocation}</span>)}
-        {urlMaps && (
-          <>
-            <DesktopButton text="Click me" url={urlMaps}/>
-            <DesktopButton text="Home" url="/#/"/>
-          </>
-        )}
-      </Banner>
+      {loading && (<Loader />)}
+      {!loading && !validSchedule && (
+        <Banner>
+          <span>Not available</span>
+          <span>Out of schedule</span>
+        </Banner>
+      )}
+      {!loading && validSchedule && (
+        <Banner>
+          <span>{errorMessage}</span>
+          {validLocation && (<span>{validLocation}</span>)}
+          {urlMaps && (
+            <>
+              <DesktopButton text="Click me" url={urlMaps}/>
+              <DesktopButton text="Home" url="/#/"/>
+            </>
+          )}
+        </Banner>
+      )}
     </div>
   )
 }
--- src/utils/app-utils.tsx
+++ src/utils/app-utils.tsx
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react';
+
 const isDayTime = () => {
     const currentDate = new Date();
     const currentHours = currentDate.getHours();
@@ -19,25 +21,46 @@ interface CurrentLocation {
 
 const checkFence = (currentLocation: CurrentLocation) => {
     const { lat, lng } = currentLocation;
+    const {
+        VITE_APP_ALPHA_LOCATION,
+        VITE_APP_BETA_LOCATION,
+        VITE_APP_CBLLA_2,
+        VITE_APP_CBLLA,
+        VITE_APP_CBLLO_2,
+        VITE_APP_CBLLO,
+        VITE_APP_CBRLA_2,
+        VITE_APP_CBRLA,
+        VITE_APP_CBRLO_2,
+        VITE_APP_CBRLO,
+        VITE_APP_CTLLA_2,
+        VITE_APP_CTLLA,
+        VITE_APP_CTLLO_2,
+        VITE_APP_CTLLO,
+        VITE_APP_CTRLA_2,
+        VITE_APP_CTRLA,
+        VITE_APP_CTRLO_2,
+        VITE_APP_CTRLO,
+    } = import.meta.env;
+    const validations = [
+        { lat: VITE_APP_CTLLA, lng: VITE_APP_CTLLO },
+        { lat: VITE_APP_CTRLA, lng: VITE_APP_CTRLO },
+        { lat: VITE_APP_CBLLA, lng: VITE_APP_CBLLO },
+        { lat: VITE_APP_CBRLA, lng: VITE_APP_CBRLO },
+    ];
+    const validations2 = [
+        { lat: VITE_APP_CTLLA_2, lng: VITE_APP_CTLLO_2 },
+        { lat: VITE_APP_CTRLA_2, lng: VITE_APP_CTRLO_2 },
+        { lat: VITE_APP_CBLLA_2, lng: VITE_APP_CBLLO_2 },
+        { lat: VITE_APP_CBRLA_2, lng: VITE_APP_CBRLO_2 },
+    ];
+    const [ topLeft, topRight, bottomLeft, bottomRight ] = validations;
+    const [ topLeft2, topRight2, bottomLeft2, bottomRight2 ] = validations2;
+
     let alphaCheck_1 = "";
     let alphaCheck_2 = "";
     let betaCheck_1 = "";
     let betaCheck_2 = "";
     let result = "";
-    const validations2 = [
-        { lat: import.meta.env.VITE_APP_CTLLA_2, lng: import.meta.env.VITE_APP_CTLLO_2 },
-        { lat: import.meta.env.VITE_APP_CTRLA_2, lng: import.meta.env.VITE_APP_CTRLO_2 },
-        { lat: import.meta.env.VITE_APP_CBLLA_2, lng: import.meta.env.VITE_APP_CBLLO_2 },
-        { lat: import.meta.env.VITE_APP_CBRLA_2, lng: import.meta.env.VITE_APP_CBRLO_2 },
-    ];
-    const validations = [
-        { lat: import.meta.env.VITE_APP_CTLLA, lng: import.meta.env.VITE_APP_CTLLO },
-        { lat: import.meta.env.VITE_APP_CTRLA, lng: import.meta.env.VITE_APP_CTRLO },
-        { lat: import.meta.env.VITE_APP_CBLLA, lng: import.meta.env.VITE_APP_CBLLO },
-        { lat: import.meta.env.VITE_APP_CBRLA, lng: import.meta.env.VITE_APP_CBRLO },
-    ];
-    const [ topLeft, topRight, bottomLeft, bottomRight ] = validations;
-    const [ topLeft2, topRight2, bottomLeft2, bottomRight2 ] = validations2;    
     
     if ( lat <= topLeft.lat && lat >= bottomLeft.lat && lat <= topRight.lat && lat >= bottomRight.lat) {
         alphaCheck_1 = "alpha";        
@@ -56,18 +79,105 @@ const checkFence = (currentLocation: CurrentLocation) => {
     }
 
     if ( alphaCheck_1 && alphaCheck_2 && alphaCheck_1 === alphaCheck_2 ) {
-        result = import.meta.env.VITE_APP_ALPHA_LOCATION;
+        result = VITE_APP_ALPHA_LOCATION;
     }
 
     if ( betaCheck_1 && betaCheck_2 && betaCheck_1 === betaCheck_2 ) {
-        result = import.meta.env.VITE_APP_BETA_LOCATION;
+        result = VITE_APP_BETA_LOCATION;
     }
 
     return result !== "" ? result : "Invalid location";
 }
 
+interface dataTime {
+    date_time: string
+}
+
+interface timeZoneTest {
+    data: dataTime,
+    loading: boolean,
+    error: string | undefined
+  }
+
+const fetchTimezone = (url:string):timeZoneTest => {
+    const [data, setData] = useState({date_time: ""});
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        fetch(url)
+        .then(response => response.json())
+        .then(setData)
+        .catch(setError)
+        .finally(() => setLoading(false));
+    }, [url]);
+
+    return { data, error, loading };
+};
+
+const validTime = (date:string) => {
+    let isValidTime = false;
+    const decodedDate = new Date(date);
+    const hour = decodedDate.getHours().toString();
+    const minutes = decodedDate.getMinutes();
+    const {
+        VITE_APP_BETA_EVENING_END_HOUR: eveningEnd,
+        VITE_APP_BETA_EVENING_START_HOUR: eveningStart,
+        VITE_APP_BETA_MORNING_END_HOUR: morningEnd,
+        VITE_APP_BETA_MORNING_START_HOUR: morningStart,
+    } = import.meta.env;
+
+    const startHour = () => {
+
+        if ( hour >= morningStart && hour <= morningEnd ) {
+            return {
+                startTime: morningStart,
+                endTime: morningEnd,
+                validWindow: true,
+            }
+        } else if ( hour >= eveningStart && hour <= eveningEnd ) {
+            return {
+                startTime: eveningStart,
+                endTime: eveningEnd,
+                validWindow: true,
+            }
+        } else {
+            return {
+                startTime: 0,
+                endTime: 0,
+                validWindow: false,
+            }
+        }
+    }
+    
+    const { startTime, endTime, validWindow } = startHour();
+
+    if ( validWindow ) {
+        if ( hour >= startTime && hour <= endTime ) {
+            const minutesLeft = 60 - minutes;
+            if ( hour < endTime || hour === endTime && minutesLeft > 15 ) {
+                isValidTime = true;
+            }
+        }
+    }
+    return isValidTime;
+}
+
+interface mapsParams {
+    latitude: number,
+    longitude: number,
+}
+
+const getMapsUrl = (params:mapsParams):string => {
+    const { latitude, longitude } = params;
+    return `https://www.google.com/maps/search/?api=1&query=${latitude}%2C${longitude}&hl=es-419`;
+}
+
 export { 
+    checkFence,
+    fetchTimezone,
+    getMapsUrl,
     isDayTime,
     setBackground,
-    checkFence,
+    validTime,
 }
